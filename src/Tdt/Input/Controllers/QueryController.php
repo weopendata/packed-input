db6923508ba133914709801c6e5212533fc58e24
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2014-07-31 17:22:32 +0200

    Small tweak to the artists query

--- src/Tdt/Input/Controllers/QueryController.php
+++ src/Tdt/Input/Controllers/QueryController.php
@@ -26,7 +26,12 @@ class QueryController extends \Controller
      */
     public function handle()
     {
-        //$parameters = array('creator', 'objectName', 'title', 'objectNumber');
+        $input = \Input::all();
+
+        // If nothing is given, return a 400
+        if (empty($input)) {
+            \App::abort(400, "Please provide a parameter with your request (objectNumber, objectName, creator, title).");
+        }
 
         $results = array();
 
@@ -35,16 +40,16 @@ class QueryController extends \Controller
 
         $creator = \Input::get('creator');
 
+        // Check if index is true or false
+        $index = \Input::get('index', false);
+
+        $index = (bool) $index;
+
         if (!empty($creator)) {
 
             $artists = $this->getCollection('artists');
 
             $works = $this->getCollection('institutions');
-
-            // Check if index is true or false
-            $index = \Input::get('index', false);
-
-            $index = (bool) $index;
 
             $filter = array(
                         'creator' => array(
@@ -84,26 +89,80 @@ class QueryController extends \Controller
 
             foreach ($artistCursor as $artist) {
 
-                // Foreach artist, search for accompanying works
-                $filter = array('creatorId' => @$result['creatorId']);
+                if (!empty($artist['creatorId'])) {
 
-                $properties = array(
-                                '_id' => 0,
-                                'dateIso8601Range' => 0
-                            );
+                    // Foreach artist, search for accompanying works
+                    $filter = array('creatorId' => $artist['creatorId']);
 
-                $worksCursor = $works->find($filter, $properties);
+                    // TODO: add the object filters !!
 
-                $artist['works'] = array();
+                    $properties = array(
+                                    '_id' => 0,
+                                    'dateIso8601Range' => 0
+                                );
 
-                foreach ($worksCursor as $work) {
-                    array_push($artist['works'], $work);
+                    $worksCursor = $works->find($filter, $properties);
+
+                    $artist['works'] = array();
+
+                    foreach ($worksCursor as $work) {
+                        array_push($artist['works'], $work);
+                    }
                 }
 
                 array_push($artistResults, $artist);
             }
 
             $results['artists'] = $artistResults;
+
+        } else {
+
+            $objectResults = array();
+
+            $parameters = array('objectNumber', 'title', 'objectName');
+
+            $filterParameters = array();
+
+            foreach ($parameters as $parameter) {
+
+                $val = \Input::get($parameter);
+
+                if (!empty($val)) {
+                    $filterParameters[$parameter] = \Input::get($parameter);
+                }
+            }
+
+            // Build the $and clause
+
+            $and = array();
+
+            foreach ($filterParameters as $filterParam => $filterVal) {
+
+                $clause = array(
+                            $filterParam => array(
+                                '$regex' => '.*' . $filterVal . '.*',
+                                '$options' => 'i'
+                            )
+                        );
+
+                array_push($and, $clause);
+            }
+
+            $filter = array($and);
+
+            // Get the objects with the build up filter
+            $objects = $this->getCollection('objects');
+
+            // Properties that don't need to be returned
+            $properties = array(
+                            '_id' => 0,
+                            'created_at' => 0,
+                            'updated_at' => 0,
+                        );
+
+            $objects->find($filter);
+
+            $results['objects'] = $objectResults;
         }
 
         // Check for object related parameters
